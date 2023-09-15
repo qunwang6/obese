a89f98792f2a5fa935a49584aed53cf44e744d1e
qunwang6 <qunwang6@gmail.com>
2023-09-15 11:25:27 +0800

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App: React.FunctionComponent = () => {
 
   return (
     <div id="second-root">
-      <Header name="BK" />
+      <Header name="⠀⠀⠀⠀⠀" />
 
       <Route path="/">
         <Inputs />
