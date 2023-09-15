421ac9d0ca225567f8dd24b7b6ee87e70051a24c
qunwang6 <qunwang6@gmail.com>
2023-09-15 10:38:20 +0800

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App: React.FunctionComponent = () => {
 
   return (
     <div id="second-root">
-      <Header name="Obese" />
+      <Header name="" />
 
       <Route path="/">
         <Inputs />
@@ -30,7 +30,7 @@ const App: React.FunctionComponent = () => {
         </Suspense>
       </Route>
 
-      <About />
+   
     </div>
   );
 };
